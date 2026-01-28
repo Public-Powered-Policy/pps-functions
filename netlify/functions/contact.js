5af47b5c61f10380a4cda2685c5e85ff3c0f60a6
rentry <brent.ryan.johnson@gmail.com>
2026-01-28 15:26:11 -0800

    Change url for custom domain

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -35,7 +35,7 @@ export async function handler(event) {
   return {
     statusCode: 302,
     headers: {
-      Location: "https://public-powered-policy.github.io/public-powered-policy/thank-you/"
+      Location: "https://publicpoweredpolicy.com/thank-you/"
     }
   };
 }
